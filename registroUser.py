f47cee3fd3ea1e38b8e5260d71b598e8ef49ee8d
krls2302 <40965119+krls2302@users.noreply.github.com>
2020-06-25 13:02:09 -0500

    Solucionando errores

--- registroUser.py
+++ registroUser.py
@@ -1,42 +1,55 @@
-#-*- coding: utf-8 -*-
 import cv2
-import os
+import os, os.path
+import imutils
+import sys
 
-if not os.path.exists('DataSet'):
-	print('Directorio creada: DataSet')
-	os.makedirs('DataSet')
+print("Bienvenido, Este Script realiza la toma de 300 fotos de la cara.")
+
+personName = input("Nombre del usuario a registrar: ").upper()
+dataPath = 'DataSet' #Cambia a la ruta donde hayas almacenado Data
+personPath = dataPath + '/' + personName
+
+# Validar si el usuario ya esta registrado
+if not os.path.exists(personPath):
+	print('Carpeta creada: ',personPath)
+	os.makedirs(personPath)
 else:
-    print('Carpeta encontrada!')   
+	print("Carpeta existente")
+	register = len(os.listdir(personPath))
+	if register == 300:
+		print("Usuario ya esta registrado")
+		sys.exit()
+	else:
+		pass
+	print("Por favor espere.....")
 
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+#cap = cv2.VideoCapture('Video.mp4')
 
-faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 count = 0
+
 while True:
-    ret,frame = cap.read()
-    frame = cv2.flip(frame,1)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    auxFrame = frame.copy()
 
-    faces = faceClassif.detectMultiScale(gray, 1.3, 5)
+	ret, frame = cap.read()
+	if ret == False: break
+	frame =  imutils.resize(frame, width=640)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	auxFrame = frame.copy()
 
-    key = cv2.waitKey(1) & 0xFF
+	faces = faceClassif.detectMultiScale(gray,1.3,5)
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y),(x+w,y+h),(128,0,255),2)
-        rostro = auxFrame[y:y+h,x:x+w]
-        rostro = cv2.resize(rostro,(150,150), interpolation=cv2.INTER_CUBIC)
-        if key == ord('g'):
-            cv2.imwrite('DataSet/rostro_{}.jpg'.format(count),rostro)
-            count = count +1
-    cv2.rectangle(frame,(10,5),(450,25),(255,255,255),-1)
-    cv2.putText(frame,'Presione g, para tomar fotos - Presione q, para salir',
-        (10,20), 2, 0.5,(255,0,0),1,cv2.LINE_AA)
-    cv2.imshow('Registro de Usuarios',frame)
+	for (x,y,w,h) in faces:
+		cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+		rostro = auxFrame[y:y+h,x:x+w]
+		rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
+		cv2.imwrite(personPath + '/rotro_{}.jpg'.format(count),rostro)
+		count = count + 1
+	cv2.imshow('Registro de Usuarios',frame)
 
-    if key == ord('q'):
-        break
-print('Se realizo el registro de {} fotos'.format(count))
+	k =  cv2.waitKey(1)
+	if k == 27 or count >= 300:
+		break
+print("Se realizo la toma de {} fotos exitosamente".format(count))
 cap.release()
 cv2.destroyAllWindows()
